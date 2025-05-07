1122384970ae9dfb266881979a6b972cee9bd764
renann_r <renann_r@hotmail.com>
2025-05-07 02:08:28 -0300

    implementado funçoes criarNovoAgendamento e atualizarStatusAgendamento para agendamento controller

--- src/controllers/agendamentoController.ts
+++ src/controllers/agendamentoController.ts
@@ -1,24 +1,54 @@
+import { isValid, parseISO } from "date-fns";
 import { Request, Response } from 'express';
 import {
+	alterarStatus,
+	criarAgendamento,
 	listarAgendamentos,
-	removerAgendamentosAntigos,
+	removerAgendamentosAntigos
 } from "../services/agendamentoService";
-import { parseISO } from "date-fns";
 
-export const criarNovoAgendamento = (req: Request, res: Response) => {};
+export const criarNovoAgendamento = (req: Request, res: Response) => {
+	const { agendamento } = req.body;
 
-export const atualizarStatusAgendamento = (req: Request, res: Response) => {};
+	try {
+		const novoAgendamento = criarAgendamento(agendamento);
+		res.status(201).json(novoAgendamento);
+	} catch (error: any) {
+		console.error('Erro ao criar agendamento:', error.message);
+		res.status(400).json({ erro: error.message || 'Erro ao criar novo agendamento' });
+	}
+};
 
-export const listarTodosAgendamentos = (req, res) => {
-	var d = req.query.data;
-	var s = req.query.status;
-	var m = req.query.motoristaCpf;
+export const atualizarStatusAgendamento = (req: Request, res: Response) => {
+	const { id, status } = req.body
+
+	try {
+		const novoStatus = alterarStatus(id, status)
+		res.status(200).send(novoStatus)
+	} catch (error: any) {
+		console.error('Erro ao criar agendamento:', error.message);
+		res.status(400).json({ erro: error.message || 'Erro ao atualizar novo status' });
+	}
+};
+
+export const listarTodosAgendamentos = (req: Request, res: Response) => {
+	const data = req.query.data as string | undefined;
+	const status = req.query.status as string | undefined;
+	const motoristaCpf = req.query.motoristaCpf as string | undefined;
 
 	let df: Date | undefined = undefined;
-	if (d) df = parseISO(d as string);
 
-	const a = listarAgendamentos(df, s, m);
-	res.status(200).json(a);
+	if (data) {
+		const parsed = parseISO(data as string);
+		if (isValid(parsed)) {
+			df = parsed;
+		} else {
+			return res.status(400).json({ erro: "Data inválida." });
+		}
+	}
+
+	const agendamentos = listarAgendamentos(df, status, motoristaCpf);
+	res.status(200).json(agendamentos);
 };
 
 export const deletarAgendamentosAntigos = (req: Request, res: Response) => {
