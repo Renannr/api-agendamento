a6905d05cd6bde137eac1c07f4eb5614722e63f3
renann_r <renann_r@hotmail.com>
2025-05-07 02:06:52 -0300

    Criado model para agendamento

--- src/models/agendamento.ts
+++ src/models/agendamento.ts
@@ -1,1 +1,11 @@
-// TODO: model do agendamento
+export type Status = 'pendente' | 'concluido' | 'atrasado' | 'cancelado';
+
+export interface Agendamento {
+  id: string;
+  dataHora: Date;
+  numeroContrato: string;
+  motoristaNome: string;
+  motoristaCpf: string;
+  placaCaminhao: string;
+  status: Status;
+}
