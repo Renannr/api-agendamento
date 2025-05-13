1af556309616438cdb5bb0fa06894c023dc6e822
renann_r <renann_r@hotmail.com>
2025-05-13 16:19:11 -0300

    fix

--- src/controllers/agendamentoController.ts
+++ src/controllers/agendamentoController.ts
@@ -8,9 +8,12 @@ import {
 } from "../services/agendamentoService";
 
 export const criarNovoAgendamento = (req: Request, res: Response) => {
-	const { agendamento } = req.body;
-
 	try {
+		const { agendamento } = req.body;
+
+		if (!agendamento) {
+			return res.status(400).json('Erro ao criar novo agendamento');
+		}
 		const novoAgendamento = criarAgendamento(agendamento);
 		res.status(201).json(novoAgendamento);
 	} catch (error: any) {
@@ -20,9 +23,11 @@ export const criarNovoAgendamento = (req: Request, res: Response) => {
 };
 
 export const atualizarStatusAgendamento = (req: Request, res: Response) => {
-	const { id, status } = req.body
-
 	try {
+		const { id, status } = req.body
+		if (!id || !status) {
+			res.status(400).json('Erro ao atualizar novo status');
+		}
 		const novoStatus = alterarStatus(id, status)
 		res.status(200).send(novoStatus)
 	} catch (error: any) {
@@ -32,26 +37,36 @@ export const atualizarStatusAgendamento = (req: Request, res: Response) => {
 };
 
 export const listarTodosAgendamentos = (req: Request, res: Response) => {
-	const data = req.query.data as string | undefined;
-	const status = req.query.status as string | undefined;
-	const motoristaCpf = req.query.motoristaCpf as string | undefined;
+	try {
+		const data = req.query.data as string | undefined;
+		const status = req.query.status as string | undefined;
+		const motoristaCpf = req.query.motoristaCpf as string | undefined;
 
-	let df: Date | undefined = undefined;
+		let df: Date | undefined = undefined;
 
-	if (data) {
-		const parsed = parseISO(data as string);
-		if (isValid(parsed)) {
-			df = parsed;
-		} else {
-			return res.status(400).json({ erro: "Data inválida." });
+		if (data) {
+			const parsed = parseISO(data);
+			if (isValid(parsed)) {
+				df = parsed;
+			} else {
+				return res.status(400).json({ erro: "Data inválida." });
+			}
 		}
-	}
 
-	const agendamentos = listarAgendamentos(df, status, motoristaCpf);
-	res.status(200).json(agendamentos);
+		const agendamentos = listarAgendamentos(df, status, motoristaCpf);
+		res.status(200).json(agendamentos);
+	} catch (error: any) {
+		console.error("Erro ao listar agendamentos:", error);
+		res.status(500).json({ erro: "Ocorreu um erro ao listar os agendamentos." });
+	}
 };
 
 export const deletarAgendamentosAntigos = (req: Request, res: Response) => {
-	removerAgendamentosAntigos();
-	res.status(204).send("Agendamentos com mais de 3 dias foram removidos");
+	try {
+		removerAgendamentosAntigos();
+		res.status(204).send("Agendamentos com mais de 3 dias foram removidos");
+	} catch (error: any) {
+		console.error("Erro ao remover agendamentos antigos:", error);
+		res.status(500).send("Ocorreu um erro ao tentar remover os agendamentos antigos");
+	}
 };
